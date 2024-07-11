3bc1ca08b197a295ed0ae5381c03f2a1d55697f9
sruthipg <sruthi15pg@gmail.com>
2024-07-11 13:56:56 +0100

    Updated Navigation graph to main activity

--- app/src/main/java/com/tapadoo/books/BookActivity.kt
+++ app/src/main/java/com/tapadoo/books/BookActivity.kt
@@ -3,6 +3,9 @@ package com.tapadoo.books
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import com.tapadoo.books.ui.view.NavGraph
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,7 +13,9 @@ class BookActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            Surface(color = MaterialTheme.colorScheme.background) {
+                NavGraph()
+            }
         }
     }
 }
